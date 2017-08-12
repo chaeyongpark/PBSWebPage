77623d435f171653789b0dd5abc638d459a266d2
Chaeyong Park <pcy8201@github.com>
2017-08-12 10:49:18 +0900

    make almost category link. change function view to class based view

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,15 +1,25 @@
 from django.conf.urls import url
 from . import views
+from homepage.views import *
 
 urlpatterns = [
     url(r'^home', views.home, name='home'),
-    url(r'^info/$', views.info, name='info'),
-    url(r'^history/$', views.history, name='history'),
-    url(r'^ci/$', views.ci, name='ci'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^pbs_plus/$', views.pbs_plus, name='pbs_plus'),
-    url(r'^card_news/$', views.card_news, name='card_news'),
-    url(r'^notice/$', views.notice, name='notice'),
+    url(r'^info/$', Info.as_view(npk = 2, cpk = 16), name='info'),
+    url(r'^history/$', History.as_view(npk = 2, cpk = 17), name='history'),
+    url(r'^ci/$', Ci.as_view(npk = 2, cpk = 18), name='ci'),
+    url(r'^contact/$', Contact.as_view(npk = 2, cpk = 21), name='contact'),
+    url(r'^news/$', ShowVideos.as_view(npk = 3, cpk = 1), name='news'),
+    url(r'^pbs_plus/$', ShowVideos.as_view(npk = 3, cpk = 2), name='pbs_plus'),
+    url(r'^poq/$', ShowVideos.as_view(npk = 3, cpk = 3), name='poq'),
+    url(r'^card_news/$', CardNews.as_view(), name='card_news'),
+    url(r'^surplus_media/$', ShowVideos.as_view(npk = 4, cpk = 5), name='surplus_media'),
+    url(r'^entertainment/$', ShowVideos.as_view(npk = 4, cpk = 6), name='entertainment'),
+    url(r'^radio/$', ShowVideos.as_view(npk = 5, cpk = 7), name='radio'),
+    url(r'^festival/$', ShowVideos.as_view(npk = 6, cpk = 8), name='festival'),
+    url(r'^sciencewar/$', ShowVideos.as_view(npk = 6, cpk = 9), name='sciencewar'),
+    url(r'^orientation/$', ShowVideos.as_view(npk = 6, cpk = 10), name='orientation'),
+    url(r'^meeting/$', ShowVideos.as_view(npk = 6, cpk = 11), name='meeting'),
+    url(r'^notice/$', Notice.as_view(), name='notice'),
     url(r'^survey/$', views.survey, name='survey'),
     url(r'^$', views.home, name='home'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -16,143 +16,108 @@ class BasicView(TemplateView):
 
         return render(request, self.template_name, {'menu': menu_list, 'current': c});
 
+def page_movement(now_str, data_types, num):
+    now = int(now_str);
+    start_point = (now - 1) * num;
+    data_type = data_types[start_point: start_point + num];
+         
+    qs = serializers.serialize('json', data_type);
+    data = json.loads(qs, encoding='utf-8');
+  
+    return { 'data': data, 'now': now};
+
 def home(request):
     recent_video = Video.objects.last()
 
     return render(request, 'homepage/home.html', {'recent_video': recent_video})
 
-def info(request):
-    menu_list = NavbarMenu.objects.get(pk=2).category.all()
-    c = menu_list.get(pk=16);
-    return render(request, 'homepage/info.html', {'menu': menu_list, 'current': c});
-
 class Info(BasicView):
-    template_name = "homepage/info.html"
+    template_name = 'homepage/info.html'
 
-def history(request):
-    menu_list = NavbarMenu.objects.get(pk=2).category.all()
-    c = menu_list.get(pk=17);
-    return render(request, 'homepage/history.html', {'menu': menu_list, 'current': c});
+class History(BasicView):
+    template_name = 'homepage/history.html'
 
-def ci(request):
-    menu_list = NavbarMenu.objects.get(pk=2).category.all()
-    c = menu_list.get(pk=18);
-    return render(request, 'homepage/ci.html', {'menu': menu_list, 'current': c});
+class Ci(BasicView):
+    template_name = 'homepage/ci.html'
 
-def contact(request):
-    menu_list = NavbarMenu.objects.get(pk=2).category.all()
-    c = menu_list.get(pk=21);
-    return render(request, 'homepage/contact.html', {'menu': menu_list, 'current': c});
+class Contact(BasicView):
+    template_name = 'homepage/contact.html'
 
-def page_movement(now_str, videos, num_of_video):
-    now = int(now_str);
-    start_point = (now - 1) * num_of_video;
-    video = videos[start_point: start_point + num_of_video];
-         
-    qs = serializers.serialize('json', video);
-    data = json.loads(qs, encoding='utf-8');
-  
-    return { 'video': data, 'now': now};
+class ShowVideos(View):
+    npk = None;
+    cpk = None;
+    
+    def get_videos(self):
+        menu_list = NavbarMenu.objects.get(pk=self.npk).category.all()
+        c = menu_list.get(pk=self.cpk)
+        videos = sorted(Video.objects.filter(category__id = c.id), key=lambda v: v.date, reverse=True)
+        page = range(1, len(videos)//6 + 2)
 
-def pbs_plus(request):
-    menu_list = NavbarMenu.objects.get(pk=3).category.all()
-    c = menu_list.get(pk=2);
-    NUM_OF_VIDEO = 6;
-    videos = sorted(Video.objects.filter(category__id = c.id), key=lambda v: v.date, reverse=True);
-    page = range(1, len(videos)//NUM_OF_VIDEO + 2);
+        return {'menu_list': menu_list, 'c': c, 'videos': videos, 'page': page}
 
-    # Page movement
-    if request.POST.get('next_page') is not None:
-    	ret = page_movement(request.POST.get('next_page'), videos, NUM_OF_VIDEO);
+    def get(self, request):
+        val = self.get_videos()
+        if request.GET.get('v') is None:
+            if len(val['videos']) is 0:
+                return render(request, 'homepage/video.html', { 'menu': val['menu_list'], 'current': val['c'], 'video': [] });
 
-	now = int(request.POST.get('next_page'));
-        start_point = (now - 1) * NUM_OF_VIDEO;
-        video = videos[start_point: start_point + NUM_OF_VIDEO];
-         
-        qs = serializers.serialize('json', video);
-        data = json.loads(qs, encoding='utf-8');
-
-	return JsonResponse({ 'video': data, 'page': page, 'now': now });
-
-    if request.GET.get('v') is None:
-        # First loading
-        if request.GET.get('p') is None:
-            m_video = videos[0];
-            video = videos[0:NUM_OF_VIDEO];
+            m_video = val['videos'][0]
+            video = val['videos'][0:6]
             now = 1;
-        
-    # Video change
-    else:
-        m_video = Video.objects.filter(pk = request.GET.get('v'))[0];
-        now = int(request.GET.get('p'));
-        start_point = (now - 1) * NUM_OF_VIDEO;
-        video = videos[start_point: start_point + NUM_OF_VIDEO];
 
-    return render(request, 'homepage/video.html', { 'menu': menu_list, 'current': c, 'video': video, 'main_video': m_video, 'page': page, 'now': now });
+        else: 
+            m_video = Video.objects.filter(pk = request.GET.get('v'))[0]
+            now = int(request.GET.get('p'))
+            start_point = (now - 1) * 6
+            video = val['videos'][start_point: start_point + 6]
 
-def notice(request):
+        return render(request, 'homepage/video.html', { 'menu': val['menu_list'], 'current': val['c'], 'video': video, 'main_video': m_video, 'page': val['page'], 'now': now });
+
+    def post(self, request):
+        val = self.get_videos()
+        ret = page_movement(request.POST.get('next_page'), val['videos'], 6)
+	return JsonResponse({ 'video': ret['data'], 'page': val['page'], 'now': ret['now'] });
+
+class Notice(View):
     menu_list = NavbarMenu.objects.get(pk=7).category.all()
     c = menu_list.get(pk=12);
     NUM_OF_NOTE = 8;
     notes = Notice.objects.all().order_by('-id');
     page = range(1, len(notes)//NUM_OF_NOTE + 2);
 
-    # Page movement
-    if request.POST.get('next_page') is not None: 
-    	now = int(request.POST.get('next_page'));
-	start_point = (now - 1) * NUM_OF_NOTE;
-	note = notes[start_point: start_point + NUM_OF_NOTE];
-	
-	qs = serializers.serialize('json', note);
-        data = json.loads(qs, encoding='utf-8');
-	
-	return JsonResponse({ 'note': data, 'page': page, 'now': now});
-	
-    # First loading
-    if request.GET.get('n') is None:
-        note = notes[0: NUM_OF_NOTE];
-        now = 1;
-	
-	return render(request, 'homepage/notice.html', { 'menu': menu_list, 'current': c, 'page': page, 'now': now, 'note': note});
+    def get(self, request):
+        if request.GET.get('n') is None:
+            note = self.notes[0: self.NUM_OF_NOTE]
+            return render(request, 'homepage/notice.html', { 'menu': self.menu_list, 'current': self.c, 'page': self.page, 'now': 1, 'note': note})
 
-    # Notice loading
-    else:
-        note = Notice.objects.get(pk = request.GET.get('n'));
+        else:
+            note = self.notes.get(pk = request.GET.get('n'))
+            return render(request, 'homepage/view_note.html', { 'menu': self.menu_list, 'current': self.c, 'note': note })
 
-	return render(request, 'homepage/view_note.html', { 'menu': menu_list, 'current': c, 'note': note });
-
+    def post(self, request):
+        ret = page_movement(request.POST.get('next_page'), self.notes, self.NUM_OF_NOTE)
+	return JsonResponse({ 'note': ret['data'], 'page': self.page, 'now': ret['now'] })
+    
 def survey(request):
     menu_list = NavbarMenu.objects.get(pk=7).category.all()
     c = menu_list.get(pk=13);
     return render(request, 'homepage/view_survey.html', {'menu': menu_list, 'current': c});
 
-def card_news(request):
+class CardNews(View):
     menu_list = NavbarMenu.objects.get(pk=3).category.all()
     c = menu_list.get(pk=4);
-    NUM_OF_NOTE = 6;
     cards = CardNews.objects.all().order_by('-id');
-    page = range(1, len(cards)//NUM_OF_NOTE + 2);
+    page = range(1, len(cards)//6 + 2);
 
-    # Page movement
-    if request.POST.get('next_page') is not None: 
-    	now = int(request.POST.get('next_page'));
-	start_point = (now - 1) * NUM_OF_NOTE;
-	card = cards[start_point: start_point + NUM_OF_NOTE];
-	
-	qs = serializers.serialize('json', card);
-        data = json.loads(qs, encoding='utf-8');
-	
+    def post(self, request):
+        ret = page_movement(request.POST.get('next_page'), self.cards, 6)
 	return JsonResponse({ 'card': data, 'page': page, 'now': now});
-	
-    # First loading
-    if request.GET.get('c') is None:
-        card = cards[0: NUM_OF_NOTE];
-        now = 1;
-        print (card[0].images.all()[0]);	
-	return render(request, 'homepage/cardnews.html', { 'menu': menu_list, 'current': c, 'page': page, 'now': now, 'card': card});
 
-    # Cardnews loading
-    else:
-        card = CardNews.objects.get(pk = request.GET.get('c'));
+    def get(self, request):
+        if request.GET.get('c') is None:
+            card = self.cards[0: 6];
+            return render(request, 'homepage/cardnews.html', { 'menu': self.menu_list, 'current': self.c, 'page': self.page, 'now': 1, 'card': card});
 
-	return render(request, 'homepage/view_cardnews.html', { 'menu': menu_list, 'current': c, 'card': card });
+        else:
+            card = self.cards.get(pk = request.GET.get('c'));
+	    return render(request, 'homepage/view_cardnews.html', { 'menu': self.menu_list, 'current': self.c, 'card': card });
