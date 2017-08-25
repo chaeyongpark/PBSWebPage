fbf6aca6b6aac4108f60562147aa5da2cad21984
Chaeyong Park <pcy8201@github.com>
2017-08-25 15:18:56 +0900

    Divide page movement card string and main.js file

--- homepage/static/js/main.js
+++ homepage/static/js/main.js
@@ -60,50 +60,6 @@ $.fn.html = function(html,callback){
 ////////////
 
 /**
- * Video card string
- */
-const cstr = [
-    "<div class=\"col s12 m6 l4\"><a class=\"a-black\" href=\"/",
-    "/?v=",
-    "&p=",
-    "\"><div class=\"card video-card hoverable\"><div class=\"card-image\"><img src=\"https://img.youtube.com/vi/",
-    "/mqdefault.jpg\"></div><div class=\"card-content\"><p>",
-    "</p><p class=\"video-date\">",
-    "년 ",
-    "월 ",
-    "일</p></div></div></a></div>",
-    "\"><div class=\"card video-card hoverable\"><div class=\"card-image\"><img src=\"https://graph.facebook.com/",
-    "/picture\"></div><div class=\"card-content\"><p>",
-];
-
-/**
- * Notice card string
- */
-const nstr = [
-    "<a href=\"/notice/?n=",
-    "\"class=\"collection-item\"><p class=\"notice-list-title\">",
-    "<span class=\"badge postech-orange white-text pulse\">New</span>",
-    "<span class=\"menu grey-text text-darken-4\">",
-    "</span></p><p class=\"notice-date\">",
-    "년 ",
-    "월 ",
-    "일 </p></a>"
-];
-
-/**
- * Cardnews card string
- */
-const cnstr = [
-    "<div class=\"col s12 m6 l4\"><a class=\"a-black\" href=\"/card_news/?c=",
-    "\"><div class=\"card card-news hoverable\"><div class=\"card-image\"><img src=\"/media/",
-    "\"></div><div class=\"card-content\"><p>",
-    "</p><p class=\"video-date\">",
-    "년 ",
-    "월 ",
-    "일</p></div></div></a></div>"
-];
-
-/**
  * Mobile side navbar menu
  */
 $(".button-collapse").sideNav();
